cc6d5f8c6c65206d898c6a30f6e3af5ba960b001
tlaverty@att.net <tlaverty@att.net>
2023-03-28 20:05:10 -0400

    model

---
+++ Data Modeling.sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Titles" (
+    "Title_ID" VARCHAR   NOT NULL,
+    "Title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "Title_ID"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "Emp_No" INT   NOT NULL,
+    "Emp_Title_ID" VARCHAR   NOT NULL,
+    "Birth_Date" VARCHAR   NOT NULL,
+    "First_Name" VARCHAR   NOT NULL,
+    "Last_Name" VARCHAR   NOT NULL,
+    "Sex" VARCHAR   NOT NULL,
+    "Hire_Date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "Emp_No"
+     )
+);
+
+CREATE TABLE "Departments" (
+    "Dept_No" VARCHAR   NOT NULL,
+    "Dept_Name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "Dept_No"
+     )
+);
+
+CREATE TABLE "Dept-Manager" (
+    "Emp_No" INT   NOT NULL,
+    "Dept_No" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "Dept-Emp" (
+    "Emp_No" INT   NOT NULL,
+    "Dept_no" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "Salaries" (
+    "Emp_No" INT   NOT NULL,
+    "Salary" INT   NOT NULL
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_Emp_Title_ID" FOREIGN KEY("Emp_Title_ID")
+REFERENCES "Titles" ("Title_ID");
+
+ALTER TABLE "Dept-Manager" ADD CONSTRAINT "fk_Dept-Manager_Emp_No" FOREIGN KEY("Emp_No")
+REFERENCES "Employees" ("Emp_No");
+
+ALTER TABLE "Dept-Manager" ADD CONSTRAINT "fk_Dept-Manager_Dept_No" FOREIGN KEY("Dept_No")
+REFERENCES "Departments" ("Dept_No");
+
+ALTER TABLE "Dept-Emp" ADD CONSTRAINT "fk_Dept-Emp_Emp_No" FOREIGN KEY("Emp_No")
+REFERENCES "Employees" ("Emp_No");
+
+ALTER TABLE "Dept-Emp" ADD CONSTRAINT "fk_Dept-Emp_Dept_no" FOREIGN KEY("Dept_no")
+REFERENCES "Departments" ("Dept_No");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_Emp_No" FOREIGN KEY("Emp_No")
+REFERENCES "Employees" ("Emp_No");
+
